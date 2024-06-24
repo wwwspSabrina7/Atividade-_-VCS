b65449af00819e6f949edc3e2ed4b92d91669886
wwwspSabrina7 <00001083562435SP@aluno.educacao.sp.gov.br>
2024-06-24 20:21:08 -0300

    Add files via upload

---
+++ arquivo_python.py
@@ -0,0 +1,7 @@
+print ("Programa de repetição até digitar 0")
+
+user = int(input("Digite um numero inteiro"))
+while user != 0:
+    print("Diferente de zero !!!!")
+    user = int(input("Digite um numero inteiro"))
+print("Fechar programa!!")
